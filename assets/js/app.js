85e31fcef71275c37ce275949e8595008c28e62f
Patrick McKinney <pnmcartography@gmail.com>
2017-04-13 16:34:11 -0400

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// storymap_data can be an URL or a Javascript object
+//var storymap_data = '//media.knightlab.com/StoryMapJS/demo/demo.json';
+var storymap_data = './assets/data/pamagicBoard.json';
+
+// certain settings must be passed within a separate options object
+var storymap_options = {
+    "language": "en",
+     "map_type": "osm:standard"    
+};
+
+var storymap = new VCO.StoryMap('storyMap', storymap_data, storymap_options);
+window.onresize = function(event) {
+    storymap.updateDisplay(); // this isn't automatic
+}
