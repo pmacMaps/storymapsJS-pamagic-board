5095423aa8624488b06948292c7fb54258a7fd9d
Patrick McKinney <pnmcartography@gmail.com>
2017-04-19 14:16:07 -0400

    basic working full page app

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,8 @@ var storymap_data = './assets/data/pamagicBoard.json';
 // certain settings must be passed within a separate options object
 var storymap_options = {
     "language": "en",
-     "map_type": "osm:standard"    
+     "map_type": "osm:standard",
+    "calculate_zoom": false    
 };
 
 var storymap = new VCO.StoryMap('storyMap', storymap_data, storymap_options);
