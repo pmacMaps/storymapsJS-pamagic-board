5aeb90677b28f6ae513421f6e8e23bd944c8ed1f
Patrick McKinney <pnmcartography@gmail.com>
2018-09-06 14:59:24 -0400

    apply chnanges to app.js file.  isolated issue with map not loading on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,35 @@
 "use strict";
 
 // storymap_data can be an URL or a Javascript object
-//var storymap_data = '//media.knightlab.com/StoryMapJS/demo/demo.json';
-var storymap_data = './assets/data/pamagicBoard.json';
+const storymap_data = './assets/data/pamagicBoard.json';
 
 // certain settings must be passed within a separate options object
-var storymap_options = {
+const storymap_options = {
     "language": "en",
      "map_type": "osm:standard",
     "calculate_zoom": false    
 };
 
-var storymap = new VCO.StoryMap('storyMap', storymap_data, storymap_options);
+const storymap = new VCO.StoryMap('storyMap', storymap_data, storymap_options);
+
 window.onresize = function(event) {
     storymap.updateDisplay(); // this isn't automatic
 }
+
+// function to add alt text to images
+function addAltTextToImage() {
+    // counter for slide number
+    const currentSlide = storymap.current_slide;
+    // image element for current slide
+    const slideImage = document.getElementsByClassName('vco-media-image')[currentSlide];
+    // text content for current slide heading
+    const headlineText = document.getElementsByClassName('vco-headline')[currentSlide].textContent;
+    // set alt text to headline text 
+    slideImage.alt = headlineText;      
+}
+
+// storymap loaded
+// this works on desktop devices. It causes map to not load on mobile
+//storymap.on("loaded", addAltTextToImage);
+// slide changes
+storymap.on("change", addAltTextToImage);
